93335810f8a69634f183667e3df589bc62cb9738
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-04-24 14:38:27 +0800

    feat: enhance calculator UI and add interactivity with `TouchableOpacity`

--- calculator-app/App.js
+++ calculator-app/App.js
@@ -1,107 +1,115 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 
 export default function App() {
   return (
-    <View style={styles.body}>
+    <SafeAreaView style={styles.body}>
 
-      <Text style={styles.historyCalculation}>123</Text>
-      <Text style={styles.currentCalculation}>123+123</Text>
+      <StatusBar style="light" />
+
+      <View style={styles.textWrapper}>
+        <Text style={styles.historyText}>123</Text>
+        <Text style={styles.resultText}>123+123</Text>
+      </View>
 
       <View style={styles.container}>
 
         <View style={styles.row}>
-          <View style={[styles.button, styles.buttonGray]}>
-            <Text style={styles.buttonText}>AC</Text>
-          </View>
-          <View style={[styles.button, styles.buttonGray]}>
-            <Text style={styles.buttonSymbol}>&#177;</Text>
-          </View>
-          <View style={[styles.button, styles.buttonGray]}>
-            <Text style={styles.buttonSymbol}>%</Text>
-          </View>
-          <View style={[styles.button, styles.buttonOrange]}>
-            <Text style={styles.buttonSymbol}>&#247;</Text>
-          </View>
+          <TouchableOpacity style={[styles.button, styles.functionButton]}>
+            <Text style={styles.numericText}>AC</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.functionButton]}>
+            <Text style={styles.functionText}>&#177;</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.functionButton]}>
+            <Text style={styles.functionText}>%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.operationButton]}>
+            <Text style={styles.operatorText}>&#247;</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.row}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>7</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>8</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>9</Text>
-
-          </View>
-          <View style={[styles.button, styles.buttonOrange]}>
-            <Text style={styles.buttonSymbol}>&#215;</Text>
-          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.operationButton]}>
+            <Text style={styles.operatorText}>&#215;</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.row}>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>4</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>5</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>6</Text>
-          </View>
-          <View style={[styles.button, styles.buttonOrange]}>
-            <Text style={styles.buttonSymbol}>&#8722;</Text>
-          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.operationButton]}>
+            <Text style={styles.operatorText}>&#8722;</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.row}>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>1</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>2</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>3</Text>
-          </View>
-          <View style={[styles.button, styles.buttonOrange]}>
-            <Text style={styles.buttonSymbol}>+</Text>
-          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.operationButton]}>
+            <Text style={styles.operatorText}>+</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.row}>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>A</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>0</Text>
-          </View>
-          <View style={styles.button}>
-            <Text style={styles.buttonSymbol}>.</Text>
-          </View>
-          <View style={[styles.button, styles.buttonOrange]}>
-            <Text style={styles.buttonSymbol}>=</Text>
-          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>A</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.numericText}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.operatorText}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.operationButton]}>
+            <Text style={styles.operatorText}>=</Text>
+          </TouchableOpacity>
         </View>
-
 
       </View>
 
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#121212',
     justifyContent: 'flex-end',
     alignItems: 'center',
 },
   container: {
     width: '95%',
     height: '60%',
+  },
+  textWrapper: {
+    width: '90%',
+    justifyContent: 'flex-end',
   },
   row: {
     flex: 1,
@@ -113,40 +121,53 @@ const styles = StyleSheet.create({
     flex: 1,
     aspectRatio: 1,
     borderRadius: '100%',
-    backgroundColor: '#1c1c1c',
+    backgroundColor: '#1E1F29',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  buttonGray: {
-    backgroundColor: '#505050'
+  functionButton: {
+    backgroundColor: '#6A6D8F'
   },
-  buttonOrange: {
-    backgroundColor: '#FF9500'
+  operationButton: {
+    backgroundColor: '#3a5fcd20',
+    borderWidth: 3,
+    borderColor: '#3A5FCD',
   },
-  buttonText: {
+  numericText: {
     flex: 1,
-    color: '#fff',
-    fontSize: 38,
+    color: '#F4F6FB',
+    fontSize: 32,
     textAlign: 'center',
     textAlignVertical: 'center',
     includeFontPadding: false,
   },
-  buttonSymbol: {
-    color: '#fff',
+  functionText: {
+    flex: 1,
+    color: '#F4F6FB',
     fontSize: 40,
     textAlign: 'center',
     textAlignVertical: 'center',
+    includeFontPadding: false,
   },
-  historyCalculation: {
+  operatorText: {
+    flex: 1,
+    color: '#AFCBFF',
+    fontSize: 40,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    includeFontPadding: false,
+  },
+  historyText: {
     color: '#505050',
     fontSize: 30,
     fontWeight: 'bold',
     marginBottom: 10,
+    textAlign: 'right',
   },
-  currentCalculation: {
-    color: '#fff',
+  resultText: {
+    color: '#D0D8F2',
     fontSize: 50,
-    fontWeight: 'bold',
     marginBottom: 10,
+    textAlign: 'right',
   },
 });
