19d7c7c7cbb602f1ec394b825ec51e8cedc0c74f
Jx1126 <147470595+Jx1126@users.noreply.github.com>
2025-04-26 15:39:20 +0800

    feat: add background grid component to RootLayout

--- web-preview/app/layout.tsx
+++ web-preview/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Squares from './components/Squares';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Squares 
+          speed={0.5} 
+          squareSize={40}
+          direction='diagonal'
+          borderColor='#fff'
+          hoverFillColor='#222'
+          />
+
         {children}
       </body>
     </html>
